e6041f7656db3224dcd60c0b46d0702c51146a49
nhutb1910275 <nhutb1910275@student.ctu.edu.vn>
2022-09-21 09:07:23 +0700

    Up 3 trang gio hang, index, quantri

---
+++ index.php
@@ -0,0 +1,5 @@
+Chia sẻ Repo với thành viên trong nhóm dự án
+Phân công công việc và yêu cầu thành viên dự án cập nhật thông tin dự án.
+Trưởng dự án cập nhật Local Repo từ Remote Repo.
+Trưởng dự án kiểm tra những thay đổi do thành viên dự án thực hiện.
+Liệt kê chi tiết phân công công việc các thành viên trong nhóm theo bảng sau. (Trưởng dự án và các thành viên)
